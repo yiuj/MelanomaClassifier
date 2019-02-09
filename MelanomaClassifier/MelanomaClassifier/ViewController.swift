0624e36059d6f913f602cce18f89436a83564382
Jonathan Yiu <yiuj@usc.edu>
2019-02-09 11:44:32 -0500

    using sample app

--- MelanomaClassifier/MelanomaClassifier/ViewController.swift
+++ MelanomaClassifier/MelanomaClassifier/ViewController.swift
@@ -7,29 +7,14 @@
 //
 
 import UIKit
-import AVFoundation
 
 class ViewController: UIViewController {
-    
-    @IBOutlet weak var previewView: UIView!
-    
-    var captureSession: AVCaptureSession?
-    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let captureDevice = AVCaptureDevice.default(for: .video)
-        
-        do {
-            let input = try AVCaptureDeviceInput(device: captureDevice!)
-            captureSession = AVCaptureSession()
-            captureSession?.addInput(input)
-            captureSession?.startRunning()
-        } catch {
-            print(error)
-        }
-        
         
     }
 
