ddc2f01a38d36ac1fc6e9b5230e27dd035d04554
Jonathan Yiu <yiuj@usc.edu>
2019-02-09 09:45:20 -0500

    camera should work

--- MelanomaClassifier/MelanomaClassifier/ViewController.swift
+++ MelanomaClassifier/MelanomaClassifier/ViewController.swift
@@ -7,19 +7,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var previewView: UIView!
+    
+    var captureSession: AVCaptureSession?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let captureDevice = AVCaptureDevice.default(for: .video)
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice!)
+            captureSession = AVCaptureSession()
+            captureSession?.addInput(input)
+            captureSession?.startRunning()
+        } catch {
+            print(error)
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
